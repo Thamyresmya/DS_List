72729a206dd0f338b639c333cbe7c8bc05b23c31
Thamyres <thamyresmia@hotmail.com>
2023-11-21 16:12:33 -0300

    irst commit

--- src/main/java/com/dev/dslist/dto/GameMinDTO.java
+++ src/main/java/com/dev/dslist/dto/GameMinDTO.java
@@ -1,2 +1,49 @@
-package com.dev.dslist.dto;public class GameMinDTO {
+package com.dev.dslist.dto;
+
+
+import com.dev.dslist.entities.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+
+    //Contrutor sem paramentro
+    public GameMinDTO(){
+
+    }
+
+    //construtor que recebe uma entidade
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    //Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
 }
--- src/main/java/com/dev/dslist/repository/GameRepository.java
+++ src/main/java/com/dev/dslist/repository/GameRepository.java
@@ -1,2 +1,10 @@
-package com.dev.dslist.repository;public class GameRepository {
+package com.dev.dslist.repository;
+
+import com.dev.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
 }
